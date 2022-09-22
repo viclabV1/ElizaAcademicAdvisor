b382c191db2a05e3dba456c5a7e776001fc25582
vicclab <vplabrie@gmail.com>
2022-09-22 04:06:00 -0500

    Made question response more robust.

--- AcademicAdvisor.py
+++ AcademicAdvisor.py
@@ -1,15 +1,23 @@
-import re   
+from random import randint, random
+import re
 
 print("> Hello, I am your acadmic advisor. What can I do for you?")
-
 inputString = ""
+#rank system used in Eliza implemented by order of elifs
+#system will assume input is in complete and grammatically correct sentences
 while True:
     outputString = "> "
     inputString = input("< ")
+    #match function means it looks at beginning of sentence, so no need for start of sentence operator
     if re.match(r"exit$", inputString):
         break
-    elif re.search(r"help", inputString):
-        outputString += "What do you need help with?"
+    #if input contains question
+    elif re.search(r"(?:Can|May) I ask(?: you)? (?:something(?: else)?|a(?:nother)? question)\?", inputString):
+        responseArray = ["What do you need help with?", "Ask away.", "Go ahead."]
+        outputString += responseArray[randint(0,len(responseArray)-1)]
+    elif re.search(r"My [A-Za-z]*.$", inputString):
+        outputString += re.sub(r"My .*\.$", r"What about your", inputString)
+        outputString += "e"
     else:
         outputString += "I don't understand."
     print(outputString)
