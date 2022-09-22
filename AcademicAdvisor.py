a07c2eedd75b2c0131829a776676469ba5d9d988
vicclab <vplabrie@gmail.com>
2022-09-22 05:21:04 -0500

    Working with capture groups.

--- AcademicAdvisor.py
+++ AcademicAdvisor.py
@@ -1,8 +1,18 @@
 from random import randint, random
 import re
 
-print("> Hello, I am your acadmic advisor. What can I do for you?")
-inputString = ""
+studentName = None
+studentGrade = None
+studentMajor = None
+studnetMinor = None
+#first the adivsor will ask basic questions that can be used later
+print("> Hello, I am your acadmic advisor. What's your first name?")
+inputString = input("< ")
+if re.search(r"My name(?:'| i)s [A-Z][a-z|'-]+\.", inputString):
+    thisMatch = re.search(r"My name(?:'| i)s ([A-Z][a-z|'-]+)\.", inputString)
+    studentName = thisMatch.lastgroup()
+    #print("> Your name is ", studentName)
+    print(studentName)
 #rank system used in Eliza implemented by order of elifs
 #system will assume input is in complete and grammatically correct sentences
 while True:
@@ -13,6 +23,7 @@ while True:
         break
     #if input contains question
     elif re.search(r"(?:Can|May) I ask(?: you)? (?:something(?: else)?|a(?:nother)? question)\?", inputString):
+        #responses shouldn't always be the same, which can be handled by having a list of responses and randomly choosing
         responseArray = ["What do you need help with?", "Ask away.", "Go ahead."]
         outputString += responseArray[randint(0,len(responseArray)-1)]
     elif re.search(r"My [A-Za-z]*.$", inputString):
